32b36e44d79fb11bc59676fe5e607d20c1036b25
Olatunde <tundeficky@gmail.com>
2023-08-13 14:43:45 -0700

    files 6

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
-# Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region     = "us-east-1"
+  shared_config_files      = ["C:\Users\Olatunde\.aws\config"]
+  shared_credentials_files = ["C:\Users\Olatunde\.aws\credentials"]
+  profile                  = "Olatunde"
 }
