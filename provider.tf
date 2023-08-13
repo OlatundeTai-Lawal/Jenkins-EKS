eff088a367b0775e3d8fae7b674cda3f0bfa97e6
Olatunde <tundeficky@gmail.com>
2023-08-13 07:18:59 -0700

    files 4

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "FICKY_AWS_ACCESS_KEY_ID"
+  secret_key = " FICKY_AWS_SECRET_ACCESS_KEY"
 }
