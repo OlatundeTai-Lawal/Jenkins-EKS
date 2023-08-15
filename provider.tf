507e9473abc1c6a9d844b05e25043a1f70f0834b
Olatunde <tundeficky@gmail.com>
2023-08-14 18:16:27 -0700

    files 7

--- provider.tf
+++ provider.tf
@@ -7,7 +7,8 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "FICKY_AWS_ACCESS_KEY_ID"
+  secret_key = "FICKY_AWS_SECRET_ACCESS_KEY"
 }
